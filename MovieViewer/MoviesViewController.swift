6fbc879f59b65cc178510bcff7467c5097e506ae
sukhrobjongolibboev <sgolibbo@mail.ccsf.edu>
2016-01-23 10:23:26 -0800

    add some features
    - add pod files
    - add .gitignore
    - add .gitattribute
    -  add MBProgressHUD

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -15,11 +15,26 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [NSDictionary]?
+//    var myRequest = [NSURLRequest]?()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         tableView.dataSource = self
         tableView.delegate = self
+        
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+        
+        
+        let spining = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        spining.labelText = "Sending"
+        spining.detailsLabelText = "Please wait"
+        
 
         // Do any additional setup after loading the view.
     
@@ -42,19 +57,53 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                             self.movies = (responseDictionary["results"] as! [NSDictionary])
                             self.tableView.reloadData()
                             
+                            MBProgressHUD.hideAllHUDsForView(self.view, animated: true)
+//                            let request = NSURLRequest(URL: url!, cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData, timeoutInterval: 10)
+                            
                     }
                 }
         });
         task.resume()
         
-        
-    }
-    
+        }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+       
+        
     }
+    
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        
+        // ... Create the NSURLRequest (myRequest) ...
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = NSURLRequest(URL: url!)
+        
+        // Configure session so that completion handler is executed on main UI thread
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (data, response, error) in
+                
+                // ... Use the new data to update the data source ...
+                
+                // Reload the tableView now that there is new data
+                self.tableView.reloadData()
+                
+                // Tell the refreshControl to stop spinning
+                refreshControl.endRefreshing()
+        });
+        task.resume()
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         
         if let movies = movies {
@@ -69,6 +118,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     }
     
+    
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
         
@@ -76,6 +127,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         let posterPath = movie["poster_path"] as! String
+        
         
         let baseUrl = "http://image.tmdb.org/t/p/w500"
         
@@ -90,7 +142,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         print("row \(indexPath.row)")
         return cell
     }
-    
     
     
 
