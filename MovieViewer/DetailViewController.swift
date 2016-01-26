12b9ffa63630ec26cd572e64d1ae45b96c42369f
sukhrobjongolibboev <sgolibbo@mail.ccsf.edu>
2016-01-25 18:19:03 -0800

    add detailViewController

---
+++ MovieViewer/DetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DetailViewController.swift
+//  MovieViewer
+//
+//  Created by Sukhrobjon Golibboev on 1/25/16.
+//  Copyright © 2016 ccsf. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    var movies: NSDictionary!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = 
+        
+        
+        
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
